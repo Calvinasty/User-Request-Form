5c71953556c5b3b30f17fa717235e081281d5703
Calvinasty <48146508+Calvinasty@users.noreply.github.com>
2021-11-05 17:01:22 +0000

    Add files via upload

--- Calbank User Request Form.php
+++ Calbank User Request Form.php
@@ -57,7 +57,12 @@
         Name of Department/Unit/Branch : <input type="text" id="Deptname" name="dname">  <p class="fdate">Date : <input type="text" id="Date" name="Date"> </p> <br> <br>
         Requesting Staff Name(Same as Official Name with HR) Designation : <br>
         <input type="text" id="Deptname" name="surname" placeholder="Surname"> &nbsp; &nbsp; <input type="text" id="Deptname" name="othernames" placeholder="Other Names"> <br><br>
-        Staff Status : <input type="checkbox" name="one" onclick="return onlyone()"> Permanent <input type="checkbox" name="one" onclick="return onlyone()">  Contract <input type="checkbox"> National Service <input type="checkbox"> Intern <input type="checkbox"> Consultant <br><br>
+        Staff Status : 
+        <input type="radio"  name="one"> Permanent 
+        <input type="radio"  name="one">  Contract 
+        <input type="radio"  name="one"> National Service 
+        <input type="radio"  name="one">Intern 
+        <input type="radio"  name="one"> Consultant <br><br>
         Profile Expiring Date : <input type="date" id="PED" name="ped">    HR/MIS/CARDS Confirmation of Expiry Date : <input type="text" id="CED" name="ced"> <br><br>
         HR T24 training confirmation
        </div> <br>
@@ -66,13 +71,34 @@
               <strong>REQUEST TYPE</strong> 
            </p>
            
-           <input type="checkbox"> T24 Access &nbsp; <strong>Hold on</strong> <br> <br>
-           Mandate Access : <input type="checkbox"> Read &nbsp; <input type="checkbox"> Write &nbsp; <input type="checkbox"> Delete &nbsp; <input type="checkbox"> Access to Domain Controller (network) &nbsp; <input type="checkbox"> E-mail &nbsp; <input type="checkbox"> Moodys &nbsp; <input type="checkbox"> Laserfiche &nbsp; <input type="checkbox"> SWIFTs &nbsp; <input type="checkbox"> AML &nbsp; <input type="checkbox"> OPAY &nbsp; <input type="checkbox"> Custody System &nbsp; <br><br>
-           Hr Software  : <input type="checkbox"> Human Resource (15,17,18,20) &nbsp; <input type="checkbox"> Payroll (15,17,18,20) <br><br>
+           <input type="radio"> T24 Access &nbsp; <strong>Hold on</strong> <br> <br>
+           Mandate Access : 
+           <input type="radio" name="two"> Read &nbsp; 
+           <input type="radio" name="two"> Write &nbsp; 
+           <input type="radio" name="two"> Delete &nbsp; 
+           <input type="radio" name="two"> Access to Domain Controller (network) &nbsp; 
+           <input type="radio" name="two"> E-mail &nbsp; 
+           <input type="radio" name="two"> Moodys &nbsp; 
+           <input type="radio" name="two"> Laserfiche &nbsp; 
+           <input type="radio" name="two"> SWIFTs &nbsp; 
+           <input type="radio" name="two"> AML &nbsp; 
+           <input type="radio" name="two"> OPAY &nbsp; 
+           <input type="radio" name="two"> Custody System &nbsp; <br><br>
+           Hr Software  : 
+           <input type="radio" name="three"> Human Resource (15,17,18,20) &nbsp; 
+           <input type="radio" name="three"> Payroll (15,17,18,20) <br><br>
            Role/Description(Type of profile): <textarea rows="4" columns="100">  </textarea> <br><br>
-           Head's Approval : <input type="checkbox"> Approved &nbsp; <input type="checkbox"> Declined &nbsp; &nbsp; Date : <input type="date"> <br><br>
-           Internal Control Approval : <input type="checkbox"> Approved &nbsp; <input type="checkbox"> Declined &nbsp; &nbsp; Date : <input type="date"> <br><br>
-           Senior Management Approval : <input type="checkbox"> Approved &nbsp; <input type="checkbox"> Declined &nbsp; &nbsp; Date : <input type="date"> &nbsp;
+           Head's Approval : 
+           <input type="radio" name="four"> Approved &nbsp; 
+           <input type="radio" name="four"> Declined &nbsp; &nbsp; Date : 
+           <input type="date"> <br><br>
+           Internal Control Approval : 
+           <input type="radio" name="four"> Approved &nbsp; 
+           <input type="radio" name="four"> Declined &nbsp; &nbsp; 
+           Date : <input type="date"> <br><br>
+           Senior Management Approval : 
+           <input type="radio" name="four"> Approved &nbsp; 
+           <input type="radio" name="four"> Declined &nbsp; &nbsp; Date : <input type="date"> &nbsp;
            
 
 
@@ -83,7 +109,7 @@
               <strong>MIS USE - Access to Domain Server</strong> 
            </p>
             
-           Head's Approval : <input type="text"> &nbsp;&nbsp; Date: <input type="date"> <br><br>
+           Head's Approval : <input type="text" > &nbsp;&nbsp; Date: <input type="date"> <br><br>
            Staff Assigned : <input type="text"> &nbsp;&nbsp; Moodys ID: <input type="type"> <br><br>
            Network ID : <input type="text"> &nbsp;&nbsp; E-mail ID: <input type="email"> <br><br>
            (Contact MIS for your Password)
@@ -132,21 +158,7 @@
 </form>
 
 <script type="text/javascript">
-        function onlyone(){
-            var  a = document.getElementsByName('one');
-            var start = 0;
-            var count;
-            for(count=0; count<a.lenght; count++){
-                if(a[count].checked==true){
-                    start = start + 1;
-                }
-            }
-            
-            if (start>=1|| start <= 0){
-                alert("Select only One");
-                
-            }
-        }
+      
     
     </script>
     
